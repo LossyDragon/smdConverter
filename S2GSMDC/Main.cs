2780444dd106a4953a0719c9746ae434dbfac3fc
Lossy <LossyDragon@users.noreply.github.com>
2017-08-26 01:53:02 -0500

    Never program late at night. Little bit of refactoring to get it out of if/else hell. Easier to follow as well.

--- S2GSMDC/Main.cs
+++ S2GSMDC/Main.cs
@@ -10,124 +10,158 @@ namespace S2GSMDC
         {
             if (args.Length < 1)
             {
-                Console.WriteLine("Either drag and drop a file onto this exe, or use it from the command line with the files to convert as arguments.\nPress any key to exit.\n");
+                Console.WriteLine("Either drag and drop a file onto this exe, or use it from the command line with the files to convert as arguments.\nPress any key to exit...");
                 Console.Read();
             }
             else
             {
+                ParseArgs(args);
+            }
 
-                int successes = 0;
-                int failures = 0;
-                foreach (string file in args)
+            Console.WriteLine("Methods tried for conversion.\n");
+            Console.WriteLine("Blender: {0}", Success.BlenSuccess);
+            Console.WriteLine("Maya Mesa: {0}", Success.MesaSuccess);
+            Console.WriteLine("Decimal Coversion: {0}", Success.DecimalPointSuccess);
+
+            Console.WriteLine("All files converted.\nPress any key to exit.");
+            Console.Read();
+        }
+
+        static class Success
+        {
+            public static bool BlenSuccess { get; set; }
+            public static bool MesaSuccess { get; set; }
+            public static bool DecimalPointSuccess { get; set; }
+        }
+
+        static void ParseArgs(string[] argFile)
+        {
+
+            String BlenderRegex = @"\d+(?<vertex> +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+) +\d+ +(?<bone>\d+) +-?\d+\.\d+";
+            String MesaRegex = @"\d+(?<vertex> +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+) +\d+ +(?<bone>\d+) +-?\d+";
+            bool triangle = false;
+            Match match;
+
+            foreach (string file in argFile)
+            {
+                Console.WriteLine("Opening {0} for conversion...", file);
+
+                using (StreamReader r = new StreamReader(file))
                 {
-                    Console.WriteLine("Opening {0} for conversion...\n", file);
-
-                    using (StreamReader r = new StreamReader(file))
+                    String output = file;
+                    output = output.Insert(output.IndexOf("."), "_converted");
+                    using (StreamWriter w = new StreamWriter(output))
                     {
-                        String output = file;
-                        output = output.Insert(output.IndexOf("."), "_fixed");
-                        using (StreamWriter w = new StreamWriter(output))
+                        string line; //String hold the current line within "file"
+                        
+                        while ((line = r.ReadLine()) != null)
                         {
-                            string line;
-                            while ((line = r.ReadLine()) != null)
+                            try
                             {
-                                Match match = 
-                                    Regex.Match(line, @"\d+(?<vertex> +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+) +\d+ +(?<bone>\d+) +-?\d+\.\d+");
+                                //Blender Source Tools 2.4.0 attempt
+                                match = Regex.Match(line, BlenderRegex);
                                 {
-                                    //First check to see if the file format is using Blender Source Tools 2.4.0
-                                    if (match.Success)
+                                    if(match.Success)
                                     {
                                         line = match.Groups["bone"].Value + match.Groups["vertex"].Value;
-                                        successes++;
-
-                                        //Console.WriteLine("Blender attempt!");
-
+                                        Success.BlenSuccess = true;
                                     }
 
-                                    /**This could be written better**/
-                                    //ELSE we'll try Maya MESA format.
-                                    else
+                                }
+                                //Maya MESA v2.1 attempt
+                                match = Regex.Match(line, MesaRegex);
+                                {
+                                    if(match.Success)
                                     {
-                                        //Remove the last bit of the regex, as Maya MESA, doesnt put a floating number at the end.
-                                        match = 
-                                            Regex.Match(line, @"\d+(?<vertex> +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+) +\d+ +(?<bone>\d+) +-?\d+");
+                                        line = match.Groups["bone"].Value + match.Groups["vertex"].Value;
+                                        Success.MesaSuccess = true;
+                                    }
+
+                                }
+                                //Decimal point modification
+                                //TODO: Could this be Regex'ed??
+                                /**
+                                 *                                                       (Not Decimal)
+                                 * Example: 0 -7.43246 -47.64437 280.3624 -0.99999 0.00000 0000000 0.94172 0.82541 1 7 1
+                                 * */
+                                match = Regex.Match(line, @"(triangles)");
+                                {
+                                    if (match.Success)
+                                        triangle = true;
+                                }
+
+                                if(triangle)
+                                {
+                                    string[] tempArray = line.Split(' ');   //Split that line into a string array.
+                                    string hasDecimal = ".";                //Placeholder
+
+                                    //Parse through that string array we made. 
+                                    for (int i = 0; i < tempArray.Length; i++)
+                                    {
+                                        //If array [i] is greater than 6 characters AND NOT containing a decimal, proceed. 
+                                        if (tempArray[i].Length > 6 && !(tempArray[i].Contains(hasDecimal)))
                                         {
-                                            //If that's correct, convert it. 
-                                            if (match.Success)
-                                            {
-                                                line = match.Groups["bone"].Value + match.Groups["vertex"].Value;
-                                                successes++;
+                                            //Regex bool to make sure we're dealing with ONLY NUMBERS.
+                                            bool containsNum = Regex.IsMatch(tempArray[i], @"\d");
 
-                                                //Console.WriteLine("MESA attempt!");
+                                            //If that line is only numbers, lets put a decimal after the first digit. 
+                                            if (containsNum)
+                                            {
+                                                tempArray[i] = tempArray[i].Insert(1, hasDecimal);
 
                                             }
-                                            //if THAT doesnt work on a line, there's probably a set of numbers without a decimal, shown below. 
-                                            /**
-                                             * The following else case should fix some lines having no decimal place
-                                             * Example: 0 -7.43246 -47.64437 280.3624 -0.99999 0.00000 0000000 0.94172 0.82541 1 7 1
-                                             * */
-                                            //We'll attempt to find that non decimal number, and fix it to have one. 
-                                            else
-                                            {
-                                                string[] tempArray = line.Split(' ');   //Split that line into a string array.
-                                                string hasDecimal = ".";                //Placeholder
+                                        }
+                                    }
 
-                                                //Parse through that string array we made. 
-                                                for (int i = 0; i < tempArray.Length; i++)
-                                                {
-                                                    //If array [i] is greater than 6 characters AND NOT containing a decimal, proceed. 
-                                                    if (tempArray[i].Length > 6 && !(tempArray[i].Contains(hasDecimal)))
-                                                    {
-                                                        //Regex bool to make sure we're dealing with ONLY NUMBERS.
-                                                        bool containsNum = Regex.IsMatch(tempArray[i], @"\d");
+                                    //Combine that string array into the string streamwriter is working with.
+                                    line = string.Join(" ", tempArray);
 
-                                                        //If that line is only numbers, lets put a decimal after the first digit. 
-                                                        if (containsNum)
-                                                        {
-                                                            tempArray[i] = tempArray[i].Insert(1, ".");
-
-                                                        }
-                                                    }
-                                                }
-
-                                                //Combine that string array into the string streamwriter is working with.
-                                                line = string.Join(" ", tempArray);
-
-                                                //Lets double check the coversion now works before writing it.
-                                                match = Regex.Match(line, @"\d+(?<vertex> +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+ +-?\d+\.\d+) +\d+ +(?<bone>\d+) +-?\d+");
-                                                {
-                                                    if (match.Success)
-                                                    {
-                                                        line = match.Groups["bone"].Value + match.Groups["vertex"].Value;
-                                                        successes++;
-                                                    }
-                                                    //Some lines may have Sci Notation on it..... >.<''
-                                                    else
-                                                    {
-                                                        //TODO: Some lines appear to have scientific notation in them. 
-                                                        //Example: 0 -53.39532 1.e-00500 13.03102 -0.62235 0.000000 0.78273 0.22154 0.76860 1 0 1
-                                                        //Attempt: A  B        C         D         E       F        G       H       H
-                                                        //Coloums C-H should check for regular numbers or Sci Notation after decimal pount.
-                                                    }
-                                                }
-                                            }
+                                    match = Regex.Match(line, MesaRegex);
+                                    {
+                                        if (match.Success)
+                                        {
+                                            line = match.Groups["bone"].Value + match.Groups["vertex"].Value;
+                                            Success.DecimalPointSuccess = true;
                                         }
                                     }
                                 }
 
-                                w.WriteLine(line);  //Write the converted line to new file.
-                            }
-                            w.Close(); //Close streamwriter
-                        }
-                        Console.WriteLine("The converted file is located at {0}\n", output);
-                    }
-                }
+                                //SciNotation bypass.
 
-                Console.WriteLine("All files converted.\nPress any key to exit.");
-                Console.WriteLine(successes + " successes.");
-                Console.WriteLine(failures + " failures.");
-                Console.Read();
+                                //A line couldnt be parsed, was it something uneeded?
+
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                Console.WriteLine("At: " + line.ToString());
+                                Console.WriteLine("Application will now exit after a Key Press");
+                                Environment.Exit(0);
+
+                            }
+                            w.WriteLine(line);
+                        }
+                        w.Close();  //Close StreamWriter
+                    }
+                    r.Close();      //Close StreamReader.
+                }
             }
         }
     }
 }
+
+//                                                    //Some lines may have Sci Notation on it..... >.<''
+//                                                    else
+//                                                    {
+//                                                        //TODO: Some lines appear to have scientific notation in them... Try and REGEX it if possible.
+//                                                        //Example: 0 -53.39532 1.e-00500 13.03102 -0.62235 0.000000 0.78273 0.22154 0.76860 1 0 1
+//                                                        //         0  1        2         3         4       5        6       7       8       9 10 11
+//                                                        //Coloums C-H should check for regular numbers or Sci Notation after decimal pount.
+//
+//                                                        if (tempArray.Length == 11)
+//                                                        {
+//                                                            for (int i = 0; i < tempArray.Length; i++)
+//                                                            {
+//                                                                Console.WriteLine(tempArray[i]);
+//                                                            }
+//                                                        }
